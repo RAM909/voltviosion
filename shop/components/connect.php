39b4ad4755aabdccf681c0ebedb1aa44006357a4
Ramesh <yadavramesh9345@gmail.com>
2025-03-25 16:30:39 +0530

    change

--- shop/components/connect.php
+++ shop/components/connect.php
@@ -1,9 +1,10 @@
 <?php
 
-$db_name = 'mysql:host=localhost;dbname=shop_db';
-$user_name = 'root';
-$user_password = '';
+$host = $_ENV['DB_HOST']; // e.g., dpg-xxxxxx-a.oregon-postgres.render.com
+$dbname = $_ENV['DB_NAME']; // e.g., voltvision_db
+$user = $_ENV['DB_USER']; // e.g., postgres
+$password = $_ENV['DB_PASS']; // The password Render generated
 
-$conn = new PDO($db_name, $user_name, $user_password);
+$conn = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
 
 ?>
